381d8409e058967fea979044d46eec73e1e2da9d
Katherine Erickson <katherine.erickson@gmail.com>
2013-11-23 16:02:37 -0500

    add global clock

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 import sys
 from db_site import Site
 from initialize_db import initialize
+import config
 
 # create list of 10 sites
 sitelist = [ Site(i) for i in range(1,11) ] 
@@ -11,8 +12,11 @@ initialize(sitelist)
 for site in sitelist:
 	site.print_site_state()
 
+
 # accept stdin input stream, line by line
 line = sys.stdin.readline()
 while line:
+	config.clock += 1
+	print "Current time:" + str(config.clock)
 	print line
 	line = sys.stdin.readline()
