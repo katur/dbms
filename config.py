671001babe742a0246b0d2d9a727cfe946a10f9a
Katherine Erickson <katherine.erickson@gmail.com>
2013-11-24 11:52:54 -0500

    move tm and sites to globals

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+from db_site import Site
+from tm import TransactionManager
+
+# global clock
 clock = 0
+
+# list of 10 sites 
+sites = [ Site(i,0) for i in range(1,11) ] 
+
+# one transaction manager
+tm = TransactionManager()
--- program.py
+++ program.py
@@ -1,18 +1,8 @@
 import sys
-import config
-from db_site import Site
-from tm import TransactionManager
+from config import clock, sites, tm
 from initialize_db import initialize
 
-# create list of 10 sites
-sites = [ Site(i,0) for i in range(1,11) ] 
-
-# create the transaction manager
-tm = TransactionManager()
-
-# according to project spec,
-# initialize site data
-# along with tm's directory
+# initialize data in sites, and tm directory
 initialize(sites, tm)
 
 for site in sites:
@@ -23,8 +13,8 @@ tm.print_directory()
 # accept stdin input stream, line by line
 line = sys.stdin.readline()
 while line:
-	config.clock += 1
-	print "Current time:" + str(config.clock)
+	clock += 1
+	print "Current time:" + str(clock)
 	instructions = line.split(";")
 	for instruction in instructions:
 		# send instructions to the tm
