14f90439b989032bb49af2746c00292d25256a8c
Katherine Erickson <katherine.erickson@gmail.com>
2013-11-29 17:41:35 -0500

    minor changes

--- program.py
+++ program.py
@@ -1,7 +1,6 @@
-import sys
+import sys, re
 import globalz
 from initialize_db import initialize
-import re
 
 # initialize data in sites, and tm directory
 initialize()
@@ -16,21 +15,20 @@ line = sys.stdin.readline()
 instructions_remaining = False
 
 while line or instructions_remaining:
-	if line:
-		print 'Reading: ' + line.strip()
-
 	globalz.clock += 1 # advance time by 1
 	print "Current time:" + str(globalz.clock)
-
+	
 	# re-try all pending instructions
 	instructions_remaining = globalz.tm.attempt_pending_instructions()
 	
-	# parse new instruction(s) from input line
-	instructions = line.split(";")
-
-	# send each instruction to tm
-	for instruction in instructions:
-		globalz.tm.process_instruction(instruction.strip())
-	
 	if line:
+		print 'Reading: ' + line.strip()
+
+		# parse new instruction(s) from input line
+		instructions = line.split(";")
+
+		# send each instruction to tm
+		for instruction in instructions:
+			globalz.tm.process_instruction(instruction.strip())
+	
 		line = sys.stdin.readline() # repeat
